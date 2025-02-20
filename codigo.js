0aa80ee973df2c1186021b4fed8222fc3f1aa32d
Alejandro <ht-vdis@live.com.mx>
2025-02-19 19:30:09 -0600

    1 tarde

--- codigo.js
+++ codigo.js
@@ -1,3 +1,20 @@
 function agregar_registro(){
-    console.log(document.getElementById("apa").value);
+
+    registro = {
+        "identificacion":document.getElementById("ide").value,
+        "apellido paterno":document.getElementById("apa").value,
+        "apellido materno":document.getElementById("ama").value,
+        "nombre":document.getElementById("nom").value,
+        "edad":document.getElementById("eda").value,
+        "observaciones":document.getElementById("obs").value
+    };
+
+    document.getElementById("ide").value = null;
+    document.getElementById("apa").value = null;
+    document.getElementById("ama").value = null;
+    document.getElementById("nom").value = null;
+    document.getElementById("eda").value = null;
+    document.getElementById("obs").value = null;
+
+    console.log(registro);
 }
